45840712b81b3478a4a322ca1271c8dd50c728e1
Tzung-Lin <3407584434@qq.com>
2019-10-19 19:12:01 +0800

    数据库获取数据

--- sparkmllib/src/main/scala/Main.scala
+++ sparkmllib/src/main/scala/Main.scala
@@ -1,7 +1,26 @@
+import com.mongodb.DBObject
+import com.mongodb.casbah.{MongoClient, MongoDB}
+import org.apache.spark.{SparkConf, SparkContext}
 
+import scala.collection.mutable.ArrayBuffer
 
 object Main {
+  val MONGO_HOST:String = "localhost"
+  val MONGO_PORT:Int = 27017
+  val MONGO_DB_NAME:String = "zhilian3"
+  val MONGO_COLLECTION_NAME:String = "basel"
+  val mongoClient:MongoClient =MongoClient(MONGO_HOST,MONGO_PORT)
+  val mongoDatabase:MongoDB=mongoClient.getDB(MONGO_DB_NAME);
+  val mongoCollection=mongoDatabase.getCollection(MONGO_COLLECTION_NAME)
+  //设置spark环境
+  val sparkConf = new SparkConf().setAppName("spark graphX").setMaster("local[2]")
+  val sc = new SparkContext(sparkConf)
+  sc.setLogLevel("ERROR")
   def main(args:Array[String]): Unit ={
-    
+
+    ar dbObjectList:ArrayBuffer[DBObject]=ArrayBuffer()
+    mongoCollection.find().forEach(x=>dbObjectList.append(x))
+
+
   }
 }
